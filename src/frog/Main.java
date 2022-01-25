16091667cd56aff15befa78e543e9a4fed48fb06
Potonika <allet.dream@gmail.com>
2022-01-25 14:40:43 +0300

    task 1

--- src/frog/Main.java
+++ src/frog/Main.java
@@ -1,14 +1,7 @@
 package frog;
-import java.sql.SQLOutput;
-import java.util.Scanner;
+
 public class Main {
-    public static void main (String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int a = Integer.valueOf(scanner.nextLine());
-        int one = 1;
-        for (int i = 1; i <= a; i++){
-            one = one*i;
-        }
-        System.out.println(one);
+    public static void main(String[] args) {
+        
     }
 }
